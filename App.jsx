6e75d741baa0a5666d764e1cb0759e1046156d99
重光青空 <shigemitsuaozora@Soras-Mac.local>
2022-01-08 23:32:06 +0900

    eslint remove

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Hello from './src/components/hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
